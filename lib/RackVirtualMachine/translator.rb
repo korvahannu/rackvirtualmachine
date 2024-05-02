5888478c3b97cff07835264c78c5b0505cccd565
Hannu Korvala <hannu.s.korvala@gmail.com>
2024-05-02 08:48:06 +0300

    Arithmetic transations for add and sub

--- lib/RackVirtualMachine/translator.rb
+++ lib/RackVirtualMachine/translator.rb
@@ -4,13 +4,13 @@ module RackVirtualMachine
   # Translates a parsed VM command into Hack assembly code
   class Translator
     SEGMENT_BASE_ADDRESSES = {
-      "local" => "LCL",
-      "argument" => "ARG",
-      "this" => "THIS",
-      "that" => "THAT",
-      "temp" => "TEMP"
+      'local' => 'LCL',
+      'argument' => 'ARG',
+      'this' => 'THIS',
+      'that' => 'THAT',
+      'temp' => 'TEMP'
     }.freeze
-    THIS_THAT = ["THIS", "THAT"]
+    THIS_THAT = %w[THIS THAT]
 
     def initialize(filename)
       @filename = filename
@@ -19,42 +19,60 @@ module RackVirtualMachine
 
     # Gets an arithmetic command as a string and translates it to Hack assembly
     def get_command_arithmetic(command)
-      if command == "add"
+      if command == 'add'
+        return <<~ARM
+          @SP
+          M=M-1
+          A=M
+          D=M
+          A=A-1
+          D=D+M
+          M=D
+        ARM
+      end
+      if command == 'sub'
+        return <<~ARM
+          @SP
+          M=M-1
+          A=M
+          D=M
+          A=A-1
+          D=D-M
+          M=D
+        ARM
+      end
+      if command == 'neg'
+        return <<~ARM
+          @SP
+          A=M-1
+          M=-M
+        ARM
+      end
+      if command == 'eq'
         return <<~ARM
         ARM
       end
-      if command == "sub"
+      if command == 'gt'
         return <<~ARM
         ARM
       end
-      if command == "neg"
+      if command == 'lt'
         return <<~ARM
         ARM
       end
-      if command == "eq"
+      if command == 'and'
         return <<~ARM
         ARM
       end
-      if command == "gt"
+      if command == 'or'
         return <<~ARM
         ARM
       end
-      if command == "lt"
+      if command == 'not'
         return <<~ARM
         ARM
       end
-      if command == "and"
-        return <<~ARM
-        ARM
-      end
-      if command == "or"
-        return <<~ARM
-        ARM
-      end
-      if command == "not"
-        return <<~ARM
-        ARM
-      end
+
       raise "Unknown arithmetic command #{command}"
     end
 
@@ -63,7 +81,7 @@ module RackVirtualMachine
     def get_command_push_pop(command_type, segment, index)
       # pushes the value of segment[index] to the stack
       if command_type == CommandTypes::PUSH
-        if segment == "constant"
+        if segment == 'constant'
           return <<~PUSHCMD
             @#{index}
             D=A
@@ -74,7 +92,7 @@ module RackVirtualMachine
             M=M+1
           PUSHCMD
         end
-        if segment == "static"
+        if segment == 'static'
           return <<~PUSHCMD
             @#{@filename}.#{@index}
             D=M
@@ -85,7 +103,7 @@ module RackVirtualMachine
             M=M+1
           PUSHCMD
         end
-        if ["local", "argument", "this", "that"].include?(segment)
+        if %w[local argument this that].include?(segment)
           return <<~PUSHCMD
             @#{SEGMENT_BASE_ADDRESSES[segment]}
             D=A
@@ -100,7 +118,7 @@ module RackVirtualMachine
             M=M+1
           PUSHCMD
         end
-        if segment == "pointer"
+        if segment == 'pointer'
           return <<~PUSHCMD
             @#{THIS_THAT[index]}
             D=M
@@ -111,7 +129,7 @@ module RackVirtualMachine
             M=M+1
           PUSHCMD
         end
-        if segment == "temp"
+        if segment == 'temp'
           return <<~PUSHCMD
             @#{SEGMENT_BASE_ADDRESSES[segment]}
             D=A
@@ -128,7 +146,7 @@ module RackVirtualMachine
         end
         # pops the stack value and stores it in segment[index]
       elsif command_type == CommandTypes::POP
-        if segment == "static"
+        if segment == 'static'
           return <<~POPCMD
             @SP
             M=M-1
@@ -138,7 +156,7 @@ module RackVirtualMachine
             M=D
           POPCMD
         end
-        if ["local", "argument", "this", "that", "temp"].include?(segment)
+        if %w[local argument this that temp].include?(segment)
           return <<~POPCMD
             @#{SEGMENT_BASE_ADDRESSES[segment]}
             D=M
@@ -155,14 +173,14 @@ module RackVirtualMachine
             M=D
           POPCMD
         end
-        if segment == "pointer"
+        if segment == 'pointer'
           return <<~POPCMD
-          @SP
-          M=M-1
-          A=M
-          D=M
-          @#{THIS_THAT[index]}
-          M=D
+            @SP
+            M=M-1
+            A=M
+            D=M
+            @#{THIS_THAT[index]}
+            M=D
           POPCMD
         end
       end
